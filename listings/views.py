08729eff2bbd40069dcf31dbb1ddb749702849a3
Yair Churi <ychuri@Yair-Churis-MacBook-Pro.local>
2020-04-04 16:12:51 -0700

    readling listing from the database and pagination

--- listings/views.py
+++ listings/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from .models import Listing
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
-def listing(request):
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listing = paginator.get_page(page)
+
+    context = {
+        'listings': paged_listing
+    }
+    return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
     print(request.path)
     return render(request, 'listings/listing.html')
     
