d53118c394bbb38e93e3104c00940a93f5acf4dc
Yair Churi <ychuri@Yair-Churis-MacBook-Pro.local>
2020-04-05 15:21:56 -0700

    addiing account registration

--- btre/static/js/main.js
+++ btre/static/js/main.js
@@ -1,2 +1,16 @@
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
+
+
+setTimeout(function(){
+    $('#message').fadeOut('slow');
+}, 3000);
+
+// var messages = document.getElementById('message')
+// for (var message in messages){
+//     print(message)
+//     setTimeout(function(){
+//         $(message).fadeOut('slow')
+//     }, 3000);
+    
+// }
