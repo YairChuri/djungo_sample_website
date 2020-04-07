e8d30cffc39c33d9b839025d4eb43f5a5b228c0c
Yair Churi <ychuri@Yair-Churis-MacBook-Pro.local>
2020-04-07 09:05:24 -0700

    remove password

--- btre/settings.py
+++ btre/settings.py
@@ -150,5 +150,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'yair.churi@gmail.com'
-EMAIL_HOST_PASSWORD = 'endlesswater1!'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
